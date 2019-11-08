36e07b80afc0879a734c96891ea0c573cf9e0916
yugang <yugang@chehang168.com>
2019-11-08 13:44:44 +0800

    https://mirrors.aliyun.com/composer/

---
+++ App/HttpController/Redis.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yugang
+ * Date: 2019-11-08
+ * Time: 11:45
+ */
+
+namespace App\HttpController;
+
+
+use EasySwoole\Http\AbstractInterface\Controller;
+
+class Redis extends Controller
+{
+
+    function index()
+    {
+        // TODO: Implement index() method.
+    }
+}
